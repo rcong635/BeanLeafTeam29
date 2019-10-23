59b26ae2e161c619ec146e1e4e3ba2f7c6e18ad3
Damiano Carrioli <carrioli@usc.edu>
2019-10-23 14:31:42 -0700

    Set initial location at USC

--- app/src/main/java/com/example/beanleafteam29/MapsActivity.java
+++ app/src/main/java/com/example/beanleafteam29/MapsActivity.java
@@ -40,8 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng USC = new LatLng(34.0224, -118.2851);
+        mMap.addMarker(new MarkerOptions().position(USC).title("Marker at USC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(USC));
+        mMap.setMinZoomPreference(16);
     }
 }
