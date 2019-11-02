64a30d2648f4ee62297ae09ba6ad63dd890b9429
richardcong <rcong@usc.edu>
2019-11-02 13:11:57 -0700

    displays hard coded route

--- app/src/main/java/com/example/beanleafteam29/MapsActivity.java
+++ app/src/main/java/com/example/beanleafteam29/MapsActivity.java
@@ -1,24 +1,23 @@
 package com.example.beanleafteam29;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
-
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Bundle;
-import com.firebase.ui.auth.AuthUI;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+//import com.google.maps.model.LatLng;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,19 +26,25 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+import com.google.maps.android.*;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, LocationListener {
@@ -96,8 +101,6 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
 
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
-        LatLng location = new LatLng(400, -118);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
         enableMyLocation();
         try {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
@@ -105,6 +108,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
 
         }
         mMap.setMinZoomPreference(16);
+        displayRoute("3607 Trousdale Pkwy, Los Angeles, CA 90089", "929-959 W Jefferson Blvd, Los Angeles, CA 90007");
 
         FirebaseUIActivity.openFbReference("some_data", this);
         if(FirebaseUIActivity.isUserLoggedIn()){
@@ -114,6 +118,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
         } else {
             FirebaseUIActivity.attachListener();
         }
+
     }
 
     @Override
@@ -258,6 +263,43 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
     private void showMissingPermissionError() {
         PermissionsUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3).setApiKey(getString(R.string.google_maps_key))
+                .setConnectTimeout(1, TimeUnit.SECONDS).setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
+
+    private void addMarkersToMap(DirectionsResult results, GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(results.routes[0].legs[0].startLocation.lat,results.routes[0].legs[0].startLocation.lng)).title(results.routes[0].legs[0].startAddress));
+        map.addMarker(new MarkerOptions().position(new LatLng(results.routes[0].legs[0].endLocation.lat,results.routes[0].legs[0].endLocation.lng)).title(results.routes[0].legs[0].startAddress).snippet(getEndLocationTitle(results)));
+    }
+
+    private void displayRoute(String origin, String destination) {
+        DateTime now = new DateTime();
+        try {
+            DirectionsResult result = DirectionsApi.newRequest(getGeoContext()).mode(TravelMode.DRIVING).origin(origin).destination(destination).departureTime(now).await();
+            addMarkersToMap(result, mMap);
+            getEndLocationTitle(result);
+            addPolyline(result, mMap);
+        } catch (ApiException ae) {
+            String message = ae.getMessage();
+        } catch (InterruptedException ie) {
+
+        } catch (IOException ioe) {
+
+        }
+    }
+
+    private String getEndLocationTitle(DirectionsResult results){
+        return  "Time :"+ results.routes[0].legs[0].duration.humanReadable + " Distance :" + results.routes[0].legs[0].distance.humanReadable;
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap map) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        map.addPolyline(new PolylineOptions().addAll(decodedPath));
     }
 
 }
