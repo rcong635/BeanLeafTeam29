90b9066fdefcd9f99e633f37d8bb82973ef5ec6a
Sina Karachiani <skarachiani@Sinas-MacBook-Pro.local>
2019-11-02 11:29:41 -0700

    no longer crashes

--- app/src/main/java/com/example/beanleafteam29/MapsActivity.java
+++ app/src/main/java/com/example/beanleafteam29/MapsActivity.java
@@ -71,7 +71,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        //addLocationButton = findViewById(R.id.Add);
+        addLocationButton = findViewById(R.id.Add);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
