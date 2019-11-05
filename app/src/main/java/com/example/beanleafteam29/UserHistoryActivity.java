bd83ddd434f9f2180e897311ed86f4a84a0ca1d6
richardcong <rcong@usc.edu>
2019-11-04 18:31:21 -0800

    add times stamp to orders

--- app/src/main/java/com/example/beanleafteam29/UserHistoryActivity.java
+++ app/src/main/java/com/example/beanleafteam29/UserHistoryActivity.java
@@ -45,7 +45,6 @@ public class UserHistoryActivity extends AppCompatActivity {
                                     noItems.setVisibility(View.INVISIBLE);
                                     for (QueryDocumentSnapshot document : task.getResult()) {
 
-                                        Map<String, Object> myData = document.getData();
                                         System.out.println(document);
 
                                         LayoutInflater inflater = getLayoutInflater();
@@ -55,11 +54,11 @@ public class UserHistoryActivity extends AppCompatActivity {
                                         TextView nameView = itemView.findViewById(R.id.ItemName);
                                         nameView.setText(name);
 
-//                                        Timestamp timeStamp = (Timestamp) myData.get("Date");
-//                                        SimpleDateFormat sfd = new SimpleDateFormat("MM/dd/yyyy   HH:mm");
-//                                        TextView timeView = itemView.findViewById(R.id.ItemTime);
-//                                        String timeString = sfd.format(timeStamp.toDate());
-//                                        timeView.setText(timeString);
+                                        Timestamp timeStamp = document.getTimestamp("Date");
+                                        SimpleDateFormat sfd = new SimpleDateFormat("MM/dd/yyyy   HH:mm");
+                                        TextView timeView = itemView.findViewById(R.id.ItemTime);
+                                        String timeString = sfd.format(timeStamp.toDate());
+                                        timeView.setText(timeString);
 
                                         String location = document.getString("LocationName");
                                         TextView locationView = itemView.findViewById(R.id.ItemLocation);
@@ -70,7 +69,7 @@ public class UserHistoryActivity extends AppCompatActivity {
                                         String priceString = "$" + price;
                                         priceView.setText(priceString);
 
-                                        double caffeine = document.getDouble("Caffeine");
+                                        long caffeine = document.getLong("Caffeine");
                                         TextView caffeineView = itemView.findViewById(R.id.ItemCaffeine);
                                         String caffeineString = caffeine + "mg caffeine";
                                         caffeineView.setText(caffeineString);
@@ -89,10 +88,6 @@ public class UserHistoryActivity extends AppCompatActivity {
                         }
                     });
         }
-
-    }
-
-    public static void getUserHistory() {
 
     }
 
