bd8ee5fbb866f092393f68d92f228b34956bfffb
Sina Karachiani <skarachiani@Sinas-MacBook-Pro.local>
2019-11-05 19:15:58 -0800

    radio button done

--- app/src/main/java/com/example/beanleafteam29/ProfileActivity.java
+++ app/src/main/java/com/example/beanleafteam29/ProfileActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
     private TextView question;
     private TextView header;
-    private TextView caffeineLevel;
-    private ProgressBar caffeineBar;
+    private TextView caffeineCaption;
+    private TextView caffeineAmount;
     private Button viewHistBtn;
+    private RadioGroup rg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +25,30 @@ public class ProfileActivity extends AppCompatActivity {
 
         header = findViewById(R.id.profileHeader);
         question = findViewById(R.id.profileQuestion);
-        caffeineLevel = findViewById(R.id.caffeineAmount);
-        caffeineBar = findViewById(R.id.profileProgressBar);
+        caffeineCaption = findViewById(R.id.caffeineAmount);
+        caffeineAmount = findViewById(R.id.profileProgressBar);
         viewHistBtn = findViewById(R.id.profileViewHistBtn);
+        rg = findViewById(R.id.profileRadioGroup);
+
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+                switch(checkedId)
+                {
+                    case R.id.radioBtnYes:
+
+                        Toast.makeText(getBaseContext(), "Yes Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioBtnNo:
+                        Toast.makeText(getBaseContext(), "No Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        });
 
 //        val name = findViewById< EditText >(R.id.profile);
 //        val header = findViewById<TextView>(R.id.profileHeader);
