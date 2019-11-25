a796b689f179b4aafbc1b71cb4f7a711575d1b79
richardcong <rcong@usc.edu>
2019-11-24 17:40:23 -0800

    test

---
+++ app/src/androidTest/java/com/example/beanleafteam29/OrderMenuActivityTest.java
@@ -0,0 +1,26 @@
+package com.example.beanleafteam29;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderMenuActivityTest {
+
+    OrderMenuActivity mActivity;
+
+    @Rule
+    public ActivityTestRule<OrderMenuActivity> mActivityRule = new ActivityTestRule(OrderMenuActivity.class);
+
+    @Before
+    public void setUp() {
+        mActivity = mActivityRule.getActivity();
+    }
+
+    @Test
+    public void testMenuItemsVis
+    }
+}
